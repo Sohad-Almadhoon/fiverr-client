f56517226c4de52a60e65420714598553a69eaa4
Sohad-Almadhoon <89332592+Sohad-Almadhoon@users.noreply.github.com>
2024-09-13 16:24:14 +0300

    final check

--- src/components/slide/Slide.jsx
+++ src/components/slide/Slide.jsx
@@ -1,13 +1,35 @@
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import Slider from "react-slick";
 import "./Slide.scss";
-import Slider from "infinite-react-carousel";
+
+const PreviousArrow = ({ onClick }) => (
+  <button className="prev-arrow" onClick={onClick}>
+    Previous
+  </button>
+);
+
+const NextArrow = ({ onClick }) => (
+  <button className="next-arrow" onClick={onClick}>
+    Next
+  </button>
+);
 
 const Slide = ({ children, slidesToShow, arrowsScroll }) => {
+  const settings = {
+    slidesToShow: slidesToShow,
+    arrows: true,
+    infinite: true,
+    speed: 500,
+    slidesToScroll: arrowsScroll,
+    prevArrow: <PreviousArrow />,
+    nextArrow: <NextArrow />,
+  };
+
   return (
     <div className="slide">
       <div className="container">
-        <Slider slidesToShow={slidesToShow} arrowsScroll={arrowsScroll}>
-          {children}
-        </Slider>
+        <Slider {...settings}>{children}</Slider>
       </div>
     </div>
   );
